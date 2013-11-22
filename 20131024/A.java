6b58bf74fd36b91493a55dc217490394d212f309
anthownee <wowo944@naver.com>
2013-11-22 17:05:01 +0900

    2013년 10월 24일 (자바6장)
    2013년 10월 24일 (자바6장)

---
+++ 20131024/A.java
@@ -0,0 +1,14 @@
+
+public class A {
+	int a;
+	int b;
+	/**
+	 *  부모 클래스 생성자 A()
+	 * @param a : 정수 값입력
+	 * @param b : 정수 값입력
+	 */
+	A(int a, int b){
+		this.a=a;
+		this.b =b;
+	}
+}
---
+++ 20131024/AccountMain.java
@@ -0,0 +1,19 @@
+
+public class AccountMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Account obj1 = new Account("000-001", "정원수", 100000);
+		obj1.deposit(50000);
+		
+		StringBuffer str = new StringBuffer("계원대학교");
+		str.insert(2, "예술");
+		str.append(": 정원수");
+		str.delete(2, 4);
+		System.out.println(str);
+		
+	}
+
+}
